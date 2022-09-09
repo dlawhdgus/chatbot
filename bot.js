769338fe6be1b59bd0c5e3868203fc6eeeee4ec5
dlawhdgus <212whdgus@gmil.com>
2022-09-09 12:23:59 +0900

    init commit

---
+++ bot.js
@@ -0,0 +1,51 @@
+
+import { ARR_DATA_SET } from "./global.js";
+
+
+
+
+const help = () => {
+    console.log(ARR_DATA_SET[0]);
+}
+
+const Korea = () => {
+    console.log(ARR_DATA_SET[1]);
+}
+
+const Usa = () => {
+    console.log(ARR_DATA_SET[2]);
+}
+
+const China = () => {
+    console.log(ARR_DATA_SET[3]);
+}
+
+const Arab = () => {
+    console.log(ARR_DATA_SET[4]);
+}
+
+const Spain = () => {
+    console.log(ARR_DATA_SET[5]);
+}
+
+const France = () => {
+    console.log(ARR_DATA_SET[6]);
+}
+
+const Russia = () => {
+    console.log(ARR_DATA_SET[7]);
+}
+
+const Japan = () => {
+    console.log(ARR_DATA_SET[8]);
+}
+
+const Portugal = () => {
+    console.log(ARR_DATA_SET[9]);
+}
+
+const Philippine = () => {
+    console.log(ARR_DATA_SET[10]);
+}
+
+export { help,Korea,Usa,China,Arab, Spain, France, Russia, Japan, Portugal, Philippine}
---
+++ main.js
@@ -0,0 +1,60 @@
+import { help,Korea,Usa,China,Arab, Spain, France, Russia, Japan, Portugal, Philippine } from "./bot.js"
+import { createRequire } from "module"
+const require = createRequire(import.meta.url)
+const readline = require('readline')
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+console.log(`
+안녕하세요 종현 챗봇 입니다.
+도움말을 보고싶다면 0,
+종료를 하려면 11 을 입력해 주세요
+`)
+
+rl.on("line", (line) => {
+    let command = line;
+    switch (command) {
+        case '0' : 
+            help()
+            break
+        case '1' : 
+            Korea()
+            break;
+        case '2' : 
+            Usa()
+            break;
+        case '3' : 
+            China()
+            break;
+        case '4' : 
+            Arab()
+            break;
+        case '5' : 
+            Spain()
+            break;
+        case '6' : 
+            France()
+            break;
+        case '7' : 
+            Russia()
+            break;
+        case '8' : 
+            Japan()
+            break;
+        case '9' : 
+            Portugal()
+            break;
+        case '10' : 
+            Philippine()
+            break;
+        case '11' : 
+            rl.close();
+            break;
+        default : 
+            console.log('숫자를 잘 못 입력하셨습니다.');
+            break;
+    }
+});
